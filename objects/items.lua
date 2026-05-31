a56acf0842af51b81765c845a28dc6a895535901
HobbitWithHat <till-degenhardt@web.de>
2026-05-31 22:56:24 +0200

    Shows hand items in hud

--- objects/items.lua
+++ objects/items.lua
@@ -5,21 +5,21 @@ M.items = {
 	[hash("stick")] = {
 		hash = hash("stick"),
 		name = "Stock",
-		sprite = hash("stick"),
+		sprite = "stick",
 		crafting = nil
 	},
 
 	[hash("stone")] = {
 		hash = hash("stone"),
 		name = "Stein",
-		sprite = hash("stone"),
+		sprite = "stone",
 		crafting = nil
 	},
 
 	[hash("spear")] = {
 		hash = hash("spear"),
 		name = "Speer",
-		sprite = hash("Objekt"),
+		sprite = "Objekt",
 		crafting = "spear"
 	}
 }
