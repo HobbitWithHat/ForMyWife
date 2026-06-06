10e045f4afa17ba1b1d88f8202374c22c4089357
HobbitWithHat <till-degenhardt@web.de>
2026-06-06 13:51:18 +0200

    player collision with tilemap

--- objects/item.go
+++ objects/item.go
@@ -5,7 +5,7 @@ components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "default_animation: \"stick\"\n"
+  data: "default_animation: \"none\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
--- player/player.go
+++ player/player.go
@@ -12,17 +12,22 @@ embedded_components {
   "  texture: \"/main/sprites.atlas\"\n"
   "}\n"
   ""
+  position {
+    z: 1.0
+  }
 }
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
-  data: "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
-  "mass: 1.0\n"
+  data: "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
   "group: \"player\"\n"
   "mask: \"interact\"\n"
   "mask: \"static\"\n"
+  "mask: \"world\"\n"
+  "mask: \"block\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
@@ -32,6 +37,7 @@ embedded_components {
   "    }\n"
   "    index: 0\n"
   "    count: 3\n"
+  "    id: \"body\"\n"
   "  }\n"
   "  data: 8.0\n"
   "  data: 8.0\n"
