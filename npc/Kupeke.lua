b62e7770fda18ad196aed59a213afa3398c8db48
HobbitWithHat <till-degenhardt@web.de>
2026-08-08 22:17:37 +0200

    New Map segments

--- npc/Kupeke.lua
+++ npc/Kupeke.lua
@@ -1,5 +1,9 @@
 local M = {}
 
-M.text = {"Hello there!", "Yey, you again!"}
+M.text = {
+	{"Hello there!", "Yey, you again!", "A third time"},
+	{"Just a second time"},
+	{"I'll talk to you again, if you leave, guess."}
+}
 
 return M
